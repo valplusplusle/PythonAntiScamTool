9f360b5430d3bc6eb2712cf3458d40ccda2095e9
throny <m.throne12@gmail.com>
2020-05-19 21:06:00 +0200

    Update makeaccount.py
    changed lib to urllib3 and made the output more useful. (if http 503 then theres something wrong with your request!)

--- makeaccount.py
+++ makeaccount.py
@@ -1,9 +1,10 @@
 import os
 import random
-import urllib
+import urllib3
 
 counter = 0
 names = []
+http = urllib3.PoolManager()
 
 print ("Reading names into Array")
 
@@ -12,9 +13,8 @@ with open("names.txt", "r") as flash:
 		names.extend(line.split())
 
 for item in names:
-	print item
-	email = item + "@gmail.com"
-	print email
-	password = item + str(random.randint(1000,9999))
-	print password	
-	urllib.urlopen('https://csgoxxmagic.pro/auth.php?doAuth=1&login='+email+'&password='+password)
+
+    email = item + "@gmail.com"
+    password = item + str(random.randint(1000,9999))
+    response = http.request('POST', 'https://dopplers.club/auth.php?doAuth=1&login='+email+'&password='+password)
+    print ("item: "+item+" email: "+email+" pass: "+password+" http_status: "+str(response.status))
